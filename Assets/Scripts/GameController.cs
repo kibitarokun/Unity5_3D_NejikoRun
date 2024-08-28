ad1e53a9263a55fea11279d186a5812e24316c12
kidsrobomouse01\東京都委託訓練校 <kunren3430@gmail.com>
2024-08-28 10:43:26 +0900

    8/28　未追跡　必要なデータ　大窪

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+
+    public NejikoController nejiko;
+    public Text scoreText;
+    public LifePanel LifePanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        int score = CalcScore();
+        scoreText.text ="Score :" + score +"m";
+
+        lifePanel.UpdateLife(nejiko.Life());
+
+
+    }
+
+    int CalScore()
+    {
+        return (int)nejiko.transform.position.z;
+    }
+
+
+}
