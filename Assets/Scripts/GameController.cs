3d23c601312db2d26a944591f8b28ddf3ba5ce94
DESKTOP-5IL38K8\miada <mia.daydream@gmail.com>
2024-09-12 15:42:38 +0900

    0912 コミット

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
 
-    public NejikoController nejiko;
+    public NejikoController nejiko; //ネジコの残ライフの情報が必要
     //public Text scoreText; これだと、テキストメッシュプロは入らない
-    public TMP_Text scoreText;
+    public TMP_Text scoreText; //TextMeshProUGUI
     public LifePanel lifePanel;
 
     // Start is called before the first frame update
@@ -29,13 +30,33 @@ public class GameController : MonoBehaviour
 
         lifePanel.UpdateLife(nejiko.Life());
 
+        //ネジコのライフが0になったらゲームオーバー
+        if (nejiko.Life() <= 0)
+        {
+            //これ以降のUpdateは止める（次のUpdateはなしにする）
+            //this.enabled(GameController自身）を止める
+            enabled = false;
 
+            //ハイスコア更新
+            if (PlayerPrefs.GetInt("HighScore") < score)
+            {
+                PlayerPrefs.SetInt("HighScore", score);
+            }
+
+            //2秒後にReturnToTitleメソッドを呼び出す
+            Invoke("ReturnToTitle", 2.0f);
+        }
     }
 
     int CalcScore()
     {
+        //ネジコの走行距離（Zのポジション）をスコアとする
         return (int)nejiko.transform.position.z;
     }
 
-
+    void ReturnToTitle()
+    {
+        //タイトルシーンに切り替え
+        SceneManager.LoadScene("Title");
+    }
 }
--- Assets/Scripts/TitleController.cs
+++ Assets/Scripts/TitleController.cs
@@ -2,14 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+
 
 public class TitleController : MonoBehaviour
 {
+    public TextMeshProUGUI highScoreText;
 
-
+    public void Start()
+    {
+        TitleScore();
+    }
+    // Start is called before the first frame update
     public void OnStartButtonClicked()
     {
         SceneManager.LoadScene("Main");
     }
+
+    //スコア表示
+    public void TitleScore()
+    {
+        highScoreText.text = "High Score : " + PlayerPrefs.GetInt("HighScore") + "m";
+    }
+
+    //リセットするボタンを用意する場合
+    public void ResetScore()
+    {
+        PlayerPrefs.SetInt("HighScore", 0);
+        TitleScore();
+    }
+
+    //ゲーム終了
+    public void EndClicked()
+    {
+        Application.Quit(); //アプリ終了
+    }
 }
-    
